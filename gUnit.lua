de9bc3259c5e84f6bb1cadad1112ad83205b8a9f
Dionisiu Brovka <d.brovka2013@yandex.ru>
2019-07-27 15:06:57 +0300

    Changes to be committed: 	renamed:    unit.lua -> gUnit.lua 	modified:   main.lua

--- gUnit.lua
+++ gUnit.lua
@@ -4,21 +4,31 @@
 
 local unit = {}
 
+
 	unit.gensArray = {}  ----------------------------------основной массив генов
 
+		unit.genN = 32
+
 		unit.iterCount = 1 --------------------------------количество генираций
+	
+	local array = unit.gensArray --------------------------сокращенная форма записи
 --===============================================================================================================
 -- Саб функции
 
-function chFind(str, pos) -- Нахождение чара по позиции 
+function chrFind(str, pos) -- Нахождение чара по позиции 
 	rez = str:sub(pos,pos)
 	return rez
+end
+
+function chrRep( str, pos, chr )
+	newStr = str:sub(1,pos-1) .. chr .. str:sub(pos+1)
+	return newStr
 end
 
 --===============================================================================================================
 -- Функции работы с датой
 
-function unit.genInit( params ) -- Создание набора генов
+function unit.genInit( params ) -- Создание генов
 	local gen = {} --{{1}}
 
 	local genStr = ""
@@ -58,8 +68,14 @@ function unit.genInit( params ) -- Создание набора генов
 	end
 end
 
-function unit.gensDataInit(params)
+function unit.gensDataInit(params) -- Создание наборов генов 
 	local data = ""
+
+		if params.genN == "" then 
+			params.genN = 32
+		end
+
+		unit.genN = params.genN
 
 		for i=1, params.genN do
 			data = data ..unit.genInit({initType = "default", flag = "-s"}) .. "0/" .."\n"
@@ -77,11 +93,10 @@ function unit.gensDataInit(params)
 	end
 end
 
-function unit.gensDataWrite()
+function unit.gensDataWrite() -- Запись данных в файл
 	local data = ""
-	local array = unit.gensArray
 
-		for i=1, unit.gensArray.counter do
+		for i=1, array.counter do
 			data = data .. string.format("%s>%s:%s/", array[i].HEXname, array[i].gen, array[i].lifeCircyle) .."\n"
 		end 
 
@@ -97,7 +112,7 @@ function unit.gensDataWrite()
 	end
 end
 
-function unit.gensDataRead()
+function unit.gensDataRead() -- Чтение данных с файла 
 	local path = system.pathForFile( "gens.txt", system.DocumentsDirectory )
 
 	local file, errorString = io.open( path, "r" )
@@ -111,14 +126,14 @@ function unit.gensDataRead()
 	        counter = counter + 1
 			unit.gensArray[counter] = {}
 
-				unit.gensArray[counter].HEXname		= string.sub(line, 1, 10)
-				unit.gensArray[counter].gen 			= string.sub(line, 12, 75)
-				unit.gensArray[counter].lifeCircyle	= tonumber(string.sub(line, 77, string.find(line, "/")-1))
+				array[counter].HEXname		= string.sub(line, 1, 10)
+				array[counter].gen 			= string.sub(line, 12, 75)
+				array[counter].lifeCircyle	= tonumber(string.sub(line, 77, string.find(line, "/")-1))
 
 				print("gen #"..counter .. "--")
-				print("HEXname 		: " .. unit.gensArray[counter].HEXname)
-				print("gen 		: " .. unit.gensArray[counter].gen)
-				print("lifeCircyle 	: " ..unit.gensArray[counter].lifeCircyle .. "\n")
+				print("HEXname 		: " .. array[counter].HEXname)
+				print("gen 		: " .. array[counter].gen)
+				print("lifeCircyle 	: " .. array[counter].lifeCircyle .. "\n")
 	    end
 
 	    unit.gensArray.counter = counter
@@ -128,18 +143,19 @@ function unit.gensDataRead()
 	end
 end
 
-function unit.dataSave()
+function unit.dataSave() -- Создание лога
 	local data = ""
-	local date = os.date("%c")
+	local date = os.date("_%H_%M_%S")
+	local dateU = os.date("-- %c")
 	local array = unit.gensArray
 
-		data = "--" .. date .. "\n\n"
+		data = "--" .. dateU .. "\n\n"
 
 		for i=1, unit.gensArray.counter do
 			data = data .. string.format("%s>%s:%s/", array[i].HEXname, array[i].gen, array[i].lifeCircyle) .."\n"
 		end 
 
-	local path = system.pathForFile("gen-" .. unit.iterCount .. ".txt", system.DocumentsDirectory )
+	local path = system.pathForFile("logs/log".. date .. ".txt", system.DocumentsDirectory )
 
 	local file, errorString = io.open( path, "w" )
 
@@ -150,5 +166,44 @@ function unit.dataSave()
 	    io.close( file )
 	end
 end
+
+function unit.bigDATA()
+	local path = system.pathForFile( "big_data.txt", system.DocumentsDirectory )
+
+	local file, errorString = io.open( path, "r" )
+
+	local bData = {} 
+
+	local nBData = ""
+
+	if not file then
+	    print( "File error: " .. errorString )
+	else
+		counter = 0
+
+	    for line in file:lines() do
+	        counter = counter + 1
+
+	        bData[counter] = line
+	    end
+    
+	    io.close( file )
+	end
+
+	local file, errorString = io.open( path, "w" )
+
+	for i=1, counter do
+		file:write ( bData[i] .."\n" )
+	end	
+
+	for i=1, array.counter do
+		file:write ( string.format("%s>%s:", array[i].HEXname, array[i].gen) .. os.date("Msk+3.0(%c)//") .. array[i].lifeCircyle .. "\n")
+	end	
+
+    io.close( file )
+end
+
+--===============================================================================================================
+-- Функции преобразования 
 
 return unit
--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
-gens = require("unit")
+gens = require("gUnit")
 
 gens.gensDataInit({genN = 32})
 gens.gensDataRead()
 gens.gensDataWrite()
 gens.dataSave()
-
+gens.bigDATA()
